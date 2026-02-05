fd62456a32524a7ca6d8519466695cf6e25c51cc
Mohit Mudgil <mohittt0011@gmail.com>
2026-02-05 16:25:11 +0530

    Change multer storage from memory to disk

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,21 +1,11 @@
 import multer from "multer";
 
-// ✅ MEMORY STORAGE (Vercel compatible)
-const storage = multer.memoryStorage();
-
-const upload = multer({ storage });
-
-// ✅ OPTIONS SAFE
-const uploadMiddleware = (req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
+const storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, file.originalname)
   }
-  upload.fields([
-    { name: 'image1', maxCount: 1 },
-    { name: 'image2', maxCount: 1 },
-    { name: 'image3', maxCount: 1 },
-    { name: 'image4', maxCount: 1 },
-  ])(req, res, next);
-};
+})
 
-export default uploadMiddleware;
+const upload = multer({ storage })
+
+export default upload
