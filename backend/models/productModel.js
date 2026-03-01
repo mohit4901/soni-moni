0b732886f0f52f78514483c2ff6237c1d0784ffa
Mohit Mudgil <mohittt0011@gmail.com>
2026-03-01 14:15:55 +0530

    Update productModel.js

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,36 +1,52 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: Array, required: true },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: Array, required: true },
 
-    category: {
-        type: String,
-        required: true,
-        enum: ["kurti", "lehenga", "gown", "saree", "suit"]
-    },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      "Office Wear",
+      "Daily Wear",
+      "Festival Wear",
+      "Bridal Wear",
+      "Suit",
+      "Kurti"
+    ]
+  },
 
-    subCategory: {
-        type: String,
-        default: ""
-    },
+  subCategory: {
+    type: String,
+    enum: [
+      "Burberry",
+      "Cotton",
+      "Organza",
+      "Silk",
+      "Georgette",
+      "Linen"
+    ],
+    default: ""
+  },
 
-    sizes: {
-        type: Array,
-        default: []   
-    },
+  sizes: {
+    type: Array,
+    default: []
+  },
 
-    colour: {
-        type: String,
-        default: ""  
-    },
+  colour: {
+    type: String,
+    default: ""
+  },
 
-    bestseller: { type: Boolean, default: false },
-    date: { type: Number, required: true }
+  bestseller: { type: Boolean, default: false },
+  date: { type: Number, required: true }
 });
 
-const productModel = mongoose.models.product || mongoose.model("product", productSchema);
+const productModel =
+  mongoose.models.product || mongoose.model("product", productSchema);
 
 export default productModel;
